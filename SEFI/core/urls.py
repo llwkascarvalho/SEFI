d378c5ab98be7eb44b780f8dac1b00b96bd5aeeb
ErisvaldoBalbino <erisvaldoapps@gmail.com>
2024-12-05 21:34:41 -0300

    fix: estrutura dos arquivos + header

--- SEFI/core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from core import views
-
-urlpatterns = [
-    path('', views.index, name='core-index'),
-]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(request, 'core/core-index.html')
+# views
+def professorIndex(request):
+    return render(request, 'core/professor-index.html')
