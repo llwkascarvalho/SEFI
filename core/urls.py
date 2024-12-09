ec8de1c9333f010c92b36a6fff55510ad0c39ac8
ErisvaldoBalbino <erisvaldoapps@gmail.com>
2024-12-09 15:13:09 -0300

    feat: fila de solicitacoes

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 # urls
 urlpatterns = [
     path('', views.professorIndex, name='professor-index'),
+    path('fila', views.fila, name='fila'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # views
 def professorIndex(request):
     return render(request, 'core/professor-index.html')
+
+def fila(request):
+    return render(request, 'core/fila.html')
