d1006666fa04b05865a7dad4b468eb2ffbc1c462
ErisvaldoBalbino <erisvaldoapps@gmail.com>
2025-01-22 17:34:59 -0300

    mudanca nas views

--- core/views.py
+++ core/views.py
@@ -4,31 +4,16 @@ from django.contrib.auth.decorators import login_required
 # views
 @login_required
 def index(request):
-    user = request.user
-    if user.is_superuser:
-        return render(request, 'core/admin/admin-index.html')
-    elif user.groups.filter(name='Professor').exists():
-        return render(request, 'core/professor-index.html')
-    elif user.groups.filter(name='Bolsista').exists():
-        return render(request, 'core/bolsista-index.html')
-    else:
-        return redirect('login')
+    return render(request, "core/index.html")
     
+@login_required
 def perfil(request):
-    user = request.user
-    
-    if user.is_superuser:
-        return render(request, 'core/perfil-usuario/admin-perfil.html', {'user': user})
-    elif user.groups.filter(name='Professor').exists():
-        return render(request, 'core/perfil-usuario/professor-perfil.html', {'user': user})
-    elif user.groups.filter(name='Bolsista').exists():
-        return render(request, 'core/perfil-usuario/bolsista-perfil.html', {'user': user})
-    else:
-        return redirect('login')
+    return render(request, "core/perfil.html")
 
+@login_required
 def estatisticas(request):
     user = request.user
     if user.is_superuser:
-        return render(request, 'core/admin/admin-estatisticas.html')
+        return render(request, "core/estatisticas.html")
     else:
-        return redirect('login')
+        return redirect("index")
