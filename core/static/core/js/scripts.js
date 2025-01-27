a3610c297523110e26512ba1c1b73644b68a3b21
Lwkas Carvalho <lwkascarvalho1@gmail.com>
2025-01-27 14:52:12 -0300

    JS: Menu com item ativo e ajuste no layout com menu ativado e desativado

--- core/static/core/js/scripts.js
+++ core/static/core/js/scripts.js
@@ -1,16 +1,31 @@
+function toggleMenu() { 
+    const menu = document.getElementById('menu'); 
+    const main = document.querySelector('main');
+    menu.classList.toggle('active'); 
 
-// Menu
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-}
+    if (menu.classList.contains('active')) { 
+        main.style.marginLeft = '0'; 
+        main.style.marginRight = '0'; 
+        main.style.margin = '0 auto'; 
+        main.style.marginTop = '150px'; 
+    } else { 
+        main.style.marginLeft = '250px'; 
+        main.style.marginTop = '150px'; 
+        main.style.paddingLeft = '50px'; 
+    } 
+} 
+document.addEventListener('DOMContentLoaded', function() { 
+    const menuItems = document.querySelectorAll('.menu a'); 
+    const currentPath = window.location.pathname;  
 
-// Evento de clique em todos os links do menu
-document.querySelectorAll('.menu a').forEach(item => {
-    item.addEventListener('click', function () {
-        
-        document.querySelectorAll('.menu a').forEach(link => link.classList.remove('active'));
-        
-        this.classList.add('active');
-    });
-});
+    menuItems.forEach(item => { 
+        if (item.getAttribute('href') === currentPath) { 
+            item.classList.add('active');  
+        } 
+
+        item.addEventListener('click', function() { 
+            menuItems.forEach(i => i.classList.remove('active')); 
+            item.classList.add('active');  
+        }); 
+    }); 
+}); 
