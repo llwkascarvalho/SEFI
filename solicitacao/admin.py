6adbdb635c3dc335f61fb364760421771ffb0aa3
Erisvaldo <erisvaldoapps@gmail.com>
2025-02-15 14:32:21 -0300

    feat: solicitacao registrada no admin

--- solicitacao/admin.py
+++ solicitacao/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from solicitacao.models import Solicitacao
 
 # Register your models here.
+
+class SolicitacaoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'data_solicitacao', 'status')
+    list_filter = ('status', 'data_solicitacao')
+    search_fields = ('usuario__username', 'status')
+    ordering = ('-data_solicitacao',)
+
+admin.site.register(Solicitacao, SolicitacaoAdmin)
