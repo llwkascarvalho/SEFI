976b120db3b44df5503d36fce3294d9edc1882d8
Erisvaldo <erisvaldoapps@gmail.com>
2025-02-17 16:19:50 -0300

    feat: createview

--- solicitacao/views.py
+++ solicitacao/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
 from core.permissions import CheckUserProfessorMixin
+from solicitacao.models import Solicitacao
+from django.urls import reverse_lazy
+from solicitacao.forms import SolicitacaoForm
+from django.contrib import messages
 
-# mudar para createview depois   
-class NovaSolicitacaoView(CheckUserProfessorMixin, LoginRequiredMixin, TemplateView): 
+class NovaSolicitacaoView(CheckUserProfessorMixin, LoginRequiredMixin, CreateView): 
+    model = Solicitacao
     template_name = "solicitacao/nova_solicitacao.html"
+    form_class = SolicitacaoForm
+    success_url = reverse_lazy('nova-solicitacao')
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        messages.success(self.request, 'Solicitação enviada com sucesso!')
+        return super().form_valid(form)
