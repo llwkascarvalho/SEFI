6d3987481ad7d5ef199550706fbe8990d6db984f
Erisvaldo <erisvaldoapps@gmail.com>
2025-02-18 19:51:07 -0300

    chore: permission para verificar se solicitacao pertence ao usuario

--- core/permissions.py
+++ core/permissions.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+from solicitacao.models import Solicitacao
+from django.core.exceptions import PermissionDenied
 
 class CheckUserAdminMixin(UserPassesTestMixin):
     def test_func(self):
@@ -7,3 +9,14 @@ class CheckUserAdminMixin(UserPassesTestMixin):
 class CheckUserProfessorMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.groups.filter(name="Professor").exists()
+    
+class CheckSolicitacaoFromUserMixin(UserPassesTestMixin):
+    def test_func(self):
+        solicitacao = self.get_object()
+        usuario = self.request.user
+        
+        return (usuario == solicitacao.usuario and 
+                solicitacao.status == solicitacao.StatusChoices.PENDENTE)
+    
+    def handle_no_permission(self):
+        raise PermissionDenied("Você não tem permissão para modificar esta solicitação.")
