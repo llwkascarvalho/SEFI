13ad7a513b9305ee562c4dea297da97d48e934fb
Erisvaldo <erisvaldoapps@gmail.com>
2025-02-18 15:34:41 -0300

    feat: url atualizar status

--- fila/urls.py
+++ fila/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from fila.views import FilaView, DetalhesView
+from fila.views import FilaView, DetalhesView, AtualizarStatusView
 
 # urls
 urlpatterns = [
     path('', FilaView.as_view(), name='fila'),
-    path('detalhes/<int:pk>/', DetalhesView.as_view(), name='detalhes')
+    path('detalhes/<int:pk>/', DetalhesView.as_view(), name='detalhes'),
+    path('detalhes/<int:pk>/atualizar-status/', AtualizarStatusView.as_view(), name='atualizar_status'),
 ]
