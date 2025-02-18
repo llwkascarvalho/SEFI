1055bbf82e037084c57efb7af72be8f3caf01eba
Erisvaldo <erisvaldoapps@gmail.com>
2025-02-18 19:42:08 -0300

    feat: urls editar e excluir solicitacao

--- solicitacao/urls.py
+++ solicitacao/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from solicitacao.views import NovaSolicitacaoView
+from solicitacao.views import NovaSolicitacaoView, SolicitacaoUpdateView, SolicitacaoDeleteView
 
 urlpatterns = [
     path('', NovaSolicitacaoView.as_view(), name='nova-solicitacao'),
+    path('editar/<int:pk>/', SolicitacaoUpdateView.as_view(), name='editar_solicitacao'),
+    path('excluir/<int:pk>/', SolicitacaoDeleteView.as_view(), name='excluir_solicitacao'),
 ]
